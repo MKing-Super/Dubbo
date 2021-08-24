cb5dc717b28101a7bb2638c60ce16dacb9f404c4
kun.ma <1216578277@qq.com>
2021-08-24 11:26:07 +0800

    添加日志

--- dubbo-learn/learn-consumer/src/main/java/pers/mk/dubbo/learn/web/LearnConsumerApplication.java
+++ dubbo-learn/learn-consumer/src/main/java/pers/mk/dubbo/learn/web/LearnConsumerApplication.java
@@ -1,6 +1,8 @@
 package pers.mk.dubbo.learn.web;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -8,9 +10,11 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({"classpath:/META-INF/spring/*.xml"})
 @SpringBootApplication
 public class LearnConsumerApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LearnConsumerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(LearnConsumerApplication.class, args);
+        LOGGER.warn(">>>>>>>>>>>>>>>>LearnConsumer server start by springboot>>>>>>>>>>>>>>>>>>>>>>");
     }
 
 }
--- dubbo-learn/learn-provider/src/main/java/pers/mk/dubbo/learn/LearnProviderApplication.java
+++ dubbo-learn/learn-provider/src/main/java/pers/mk/dubbo/learn/LearnProviderApplication.java
@@ -1,6 +1,8 @@
 package pers.mk.dubbo.learn;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -8,9 +10,11 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({"classpath:/META-INF/spring/*.xml"})
 @SpringBootApplication
 public class LearnProviderApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LearnProviderApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(LearnProviderApplication.class, args);
+        LOGGER.warn(">>>>>>>>>>>>>>>>LearnProvider server start by springboot>>>>>>>>>>>>>>>>>>>>>>");
     }
 
 }
