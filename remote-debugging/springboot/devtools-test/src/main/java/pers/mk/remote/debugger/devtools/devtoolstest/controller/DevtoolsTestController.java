98c30237c8f8fb4401f62e8e028e2802a943796f
kun.ma <1216578277@qq.com>
2021-10-27 20:28:08 +0800

    远程devtools初次成功

--- remote-debugging/springboot/devtools-test/src/main/java/pers/mk/remote/debugger/devtools/devtoolstest/controller/DevtoolsTestController.java
+++ remote-debugging/springboot/devtools-test/src/main/java/pers/mk/remote/debugger/devtools/devtoolstest/controller/DevtoolsTestController.java
@@ -1,4 +1,16 @@
 package pers.mk.remote.debugger.devtools.devtoolstest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/devtools")
 public class DevtoolsTestController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello world";
+    }
 }
